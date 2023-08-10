713c57ec8b4dde82cf2d33c4aa281624e1ae110b
test-testperson <98484229+test-testperson@users.noreply.github.com>
2023-08-09 21:30:02 -0400

    Add repository function

---
+++ repository.m
@@ -0,0 +1,5 @@
+function metadata = repository
+metadata.owner = "test-testperson";
+metadata.name = "sample-releases-app-installer";
+metadata.url = "https://github.com/test-testperson/sample-releases-app-installer";
+end
